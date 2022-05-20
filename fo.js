275a1eff246e82cb2d28c0cd0a1a1488e1647c6a
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-05-20 22:50:12 +0530

    implement tree view

--- fo.js
+++ fo.js
@@ -15,7 +15,7 @@ let types = {
 
 switch(command){
     case 'tree':
-        treeFn();
+        treeFn(input[1]);
         break;
     case 'organize':
         organizeFn(input[1]);
@@ -27,8 +27,32 @@ switch(command){
         console.log('Please enter a valid command')
 }
 
-function treeFn(){
-console.log("tree fn implemented")
+function treeFn(dirPath){
+    if (dirPath == undefined){
+        console.log("Please enter a valid Path")
+    }else{
+        let doseExist =fs.existsSync(dirPath)
+        if(doseExist){
+            treeHelper(dirPath, " ")
+        }
+    }
+}
+
+function treeHelper(dirPath, indent){
+    let isFile = fs.lstatSync(dirPath).isFile()
+    if(isFile){
+            let fileName = path.basename(dirPath)
+            console.log(indent + " |-- "+ fileName)
+    }else{
+        let dirName = path.basename(dirPath)
+        console.log(indent + " |-- "+ dirName)
+        let children = fs.readdirSync(dirPath)
+        for(let i=0; i<children.length; i++){
+            let childrenPath = path.join(dirPath,children[i])
+            treeHelper(childrenPath, indent+'\t')
+            
+        }
+    }
 }
 
 function organizeFn(dirPath){
